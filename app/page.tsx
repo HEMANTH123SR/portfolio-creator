553c16512baf55c6147bfe48fc292de032281646
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-05-28 20:52:20 +0530

    add headerNav a navigation component and a landing page background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-
+import { HeaderNav } from "@/components/headerNav";
 
 export default function Home() {
   return (
-    <main></main>
-  );
+    <main className="flex h-full w-full flex-col">
+      <div className="flex h-screen w-full flex-col bg-[url('https://utfs.io/f/91812889-3b26-4282-a9a4-8f672ce87655-a3nf98.png')] bg-cover bg-center ">
+        <HeaderNav />
+      </div>
+
+    </main>)
 }
